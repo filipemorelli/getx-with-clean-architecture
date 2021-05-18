1cd7d568bf00d44ce8142677a1d6c1387c8989b3
Filipe Morelli <morellitecinfo@gmail.com>
2021-05-17 21:41:36 -0300

    Refactor

---
+++ test/features/home/data/repositories/user_repository_impl_test.dart
@@ -0,0 +1,28 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:get_test/features/home/data/datasources/user_datasources.dart';
+import 'package:get_test/features/home/data/models/user_model.dart';
+import 'package:get_test/features/home/data/repositories/user_repository_impl.dart';
+import 'package:get_test/features/home/domain/entities/pagination_filter_entity.dart';
+import 'package:get_test/features/home/errors/user_repository_failure.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import './user_repository_impl_test.mocks.dart';
+
+@GenerateMocks(<Type>[UserDatasource])
+void main() {
+  late final MockUserDatasource datasource;
+  late final UserRepositoryImpl repository;
+
+  setUp(() {
+    datasource = MockUserDatasource();
+    repository = UserRepositoryImpl(datasource);
+  });
+
+  test('should return a list of UserModel', () async {
+    when(datasource.getUsers(any)).thenAnswer((_) async => <UserModel>[]);
+    final result = await repository.findAll(PaginationFilterEntity(page: 1, limit: 15));
+    expect(result.isRight(), true);
+    expect(result.fold(id, id), isA<UserRepositoryFailure>());
+  });
+}
